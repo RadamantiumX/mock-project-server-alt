2bc5a9be6ca47bbf680890eaf0193701764cc5a3
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-22 00:06:29 -0300

    no changes

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,13 +1,16 @@
 import 'dotenv/config'
 import jwt from 'jsonwebtoken'
 import { IPayload } from 'types'
+import { Response } from 'express'
+
+
 
 const SECRET = process.env.JWT_SECRET || 'secret'
 
 
 export default {
     sign: (payload: IPayload) => 
-       jwt.sign(payload, SECRET, {expiresIn: '1h', algorithm: 'HS256'}),
+       jwt.sign(payload, SECRET, {expiresIn: '60s', algorithm: 'HS256'}),
 
     verify: (token: string) => jwt.verify(token, SECRET),   
 }
