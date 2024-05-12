adef1e00a78c8ac5ef9c1e23ddbae4286508d44b
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-05-12 00:11:27 -0300

    new controller

---
+++ src/controllers/message.controller.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from "http-status-codes";
+import { prisma } from "../prisma-db/prisma";
+import { validateMessageSchema } from '../schemas/validations'
+
+export class MessageController{
+    async message(req:Request, res:Response, next: NextFunction){
+        const { name, email, message } = req.body
+        try{
+          const validateMessage = validateMessageSchema(req.body)
+          if(!validateMessage.success){
+            return next({
+                status: StatusCodes.BAD_REQUEST,
+                message: JSON.parse(validateMessage.error.message)
+              })
+          }
+
+          const newMessage = await prisma.message.create({
+            data:{
+                name: name,
+                email: email,
+                message: message
+            }
+          })
+
+          res.status(StatusCodes.OK).json({ message: `Thanks for the message ${name}` })
+
+        }catch(err){
+            return next({
+                status: StatusCodes.BAD_REQUEST,
+                message: "Something's wrong"
+            })
+        }
+    }
+
+    async allMessages(req:Request, res:Response, next: NextFunction){
+        try{
+            const messages = await prisma.message.findMany({ orderBy:{ createdAt: 'desc' } })
+            if(!messages){
+                res.status(StatusCodes.OK).json({ message: 'No messages in the box' })
+            }
+
+            res.status(StatusCodes.OK).json({ data: messages })
+
+        }catch(err){
+            return next({
+                status: StatusCodes.BAD_REQUEST,
+                message: "Something's wrong"
+            })
+        }
+    }
+}
