aa7f7785e65b843ce8c5a81b86f27e65d12fe8e6
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-07-13 23:57:04 -0300

    adding routes for user controllers

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -5,5 +5,7 @@ const userRouter = Router()
 const userController = new UserController()
 
 userRouter.post('/user-info', userController.getUserInfo)
+userRouter.post('/user-update',userController.updateUserInfo)
+userRouter.post('/user-delete', userController.deleteUser)
 
 export default userRouter
