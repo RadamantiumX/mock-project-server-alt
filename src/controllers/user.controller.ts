ec9cab54511d251fa6cb7e4b40349cbf8d6ae564
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-07-13 23:57:27 -0300

    adding controller and fixing errors handling

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,19 +1,23 @@
-import bcrypt from "bcryptjs"
+//import bcrypt from "bcryptjs"
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from "http-status-codes";
 import { prisma } from "../prisma-db/prisma";
 import jwt from "../utils/jwt";
-import { validateUserSchema } from "../schemas/validations"
+// import { validateUserSchema } from "../schemas/validations"
 
 export class UserController {
     async getUserInfo (req: Request, res: Response, next:NextFunction){
-        const {token} = req.body
+        
         try{
+             const {token} = req.body
              const decode:any = jwt.verify(token)
              const email = decode.email
              const verifyUser = await prisma.user.findUnique({ where:{email} })
              if (!verifyUser){
-              return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Not valid user' })
+              return next({
+                 status: StatusCodes.UNAUTHORIZED,
+                 message: 'Not Authorized'
+              })
              }
 
              res.status(StatusCodes.OK).json({nickname: verifyUser?.nickname, email: verifyUser?.email})
@@ -21,27 +25,64 @@ export class UserController {
         }catch(error){
             return next({
                 status: StatusCodes.BAD_REQUEST,
-                message: JSON.parse('Something was wrong!')
+                message: 'Something was wrong!'
              })
         }
     }
 
     async updateUserInfo (req: Request, res: Response, next:NextFunction) {
-        const {newEmail, newNickname, token} = req.body
         try{
+            const {payload, field, token} = req.body
             const decode:any = jwt.verify(token)
              const email = decode.email
              const verifyUser = await prisma.user.findUnique({ where:{email} })
              if (!verifyUser){
-              return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Not valid user' })
+              return next({
+                status: StatusCodes.BAD_REQUEST,
+                message: 'Something was wrong!'
+             }) 
              }
-
+          if(field.includes('nickname')){
+            const updateNickname = await prisma.user.update({
+                where: {
+                    email: email
+                },
+                data: {
+                    nickname: payload
+                }
+            })
+            res.status(StatusCodes.OK).json({ message: 'The nickname was updated!' })
+          }else{
+             const updateEmail = await prisma.user.update({
+                where: {
+                    email: email
+                },
+                data: {
+                    email: payload
+                }
+             })
+             res.status(StatusCodes.OK).json({ message: 'The email was updated... You must Sign In again...' }).clearCookie('jwt')
+          }
              
 
         }catch(error){
             return next({
                 status: StatusCodes.BAD_REQUEST,
-                message: JSON.parse('Something was wrong!')
+                message: 'Something was wrong!'
+             }) 
+        }
+    }
+
+    async deleteUser(req: Request, res: Response, next:NextFunction){
+        try{
+            const {id} = req.body
+            const deleteCurrentUser = await prisma.user.delete({ where:{id: id} })
+            res.status(StatusCodes.OK).json({ message: 'User deleted...' })
+
+        }catch(error){
+            return next({
+                status: StatusCodes.BAD_REQUEST,
+                message: 'Something was wrong!'
              }) 
         }
     }
