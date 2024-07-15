3ee5caed4fb25453450d986271bd65553958864b
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-07-15 20:00:12 -0300

    deleting

--- src/schemas/validations.ts
+++ src/schemas/validations.ts
@@ -1,8 +1,8 @@
 import z from 'zod'
 import { AuthInput, PostInput, Message, UserUpdateInput } from 'types'
 
-
-type UpdatedUser = Pick<UserUpdateInput,"email" | "nickname">
+type UpdateUserPassword = Pick<AuthInput, "password" | "confirmPassword">
+type UpdatedUserInfo = Pick<UserUpdateInput,"email" | "nickname">
 // Minimum 8 characters, at least one uppercase letter,
 // one lowercase letter, one number and one special character
 const passwordValidation = new RegExp(
@@ -20,7 +20,7 @@ const userSchema = z.object({
         message: 'Please entry a valid email address'
     }),
     password: z.string().min(6, {message: 'The password must be at 6 characters minimum'}).max(30,{message: 'Is too long password'}), 
-    confirmPassword: z.string().min(6, {message: 'The confimation of password not match...'}).max(30, {message:'The confimation of password not match...'})
+    confirmPassword: z.string().min(6, {message: 'The password confirmation not match...'}).max(30, {message:'The confimation of password not match...'})
    
 }).refine((values) =>{
         return values.password === values.confirmPassword
@@ -30,18 +30,6 @@ const userSchema = z.object({
         path: ["confirmPassword"]
     }
 )
-
-const updateUserSchema = z.object({
-    nickname: z.string({
-        invalid_type_error: 'The nickname must be a valid alphanumeric characters',
-        required_error: 'The nickname is required'
-    }).min(5,{
-        message: 'The nickname must be larger than 5 characters minimum.'
-    }).refine((s)=>!s.includes(' '), 'Incorrect nickname'),
-    email: z.string().email({
-        message: 'Please entry a valid email address'
-    })
-})
 
 
 
@@ -54,7 +42,18 @@ const nickname = z.string({
 .refine((s)=>!s.includes(' '), 'Incorrect nickname')
 
 const email = z.string().email({message: 'Please entry a valid email address'})
-    
+
+const passwordSchema = z.object({
+    password: z.string().min(6, {message: 'The password must be at 6 characters minimum'}).max(30,{message: 'Is too long password'}), 
+    confirmPassword: z.string().min(6, {message: 'The password confirmation not match...'}).max(30, {message:'The confimation of password not match...'})
+}).refine((values) =>{
+    return values.password === values.confirmPassword
+},
+{
+    message: "Password must match!",
+    path: ["confirmPassword"]
+}
+)
 
 const postSchema = z.object({
     content: z.string({
@@ -84,11 +83,15 @@ export function validateUserSchema(input: AuthInput){
     return userSchema.safeParse(input)
 }
 
-export function validateNickname(input: UpdatedUser){
+export function validatePasswordSchema(input:UpdateUserPassword){
+    return passwordSchema.safeParse(input)
+}
+
+export function validateNickname(input: UpdatedUserInfo){
     return nickname.safeParse(input)
 }
 
-export function validateEmail(input: UpdatedUser){
+export function validateEmail(input: UpdatedUserInfo){
     return email.safeParse(input)
 }
 
