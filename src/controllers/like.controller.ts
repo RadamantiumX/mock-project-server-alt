35e54f2e3004de40d45bf1440534d87dacb94f3b
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-09-01 16:14:51 -0300

    change identificator

--- src/controllers/like.controller.ts
+++ src/controllers/like.controller.ts
@@ -118,7 +118,7 @@ export class LikeController {
             }
 
             res.status(StatusCodes.OK).json({ totalCount: totalCount, countLikes: likedVideo })*/
-            const total = await prisma.likeVideo.count({ where: {videoId} })
+            const total = await prisma.likeVideo.count({ where: {videoId : videoId} })
             res.status(StatusCodes.OK).json({ totalLikes: total })
 
         }catch(err){
